cc7afac559616734efc5835a9b51f1c12ef5736c
oziev02 <magomed.oziev2002@gmail.com>
2025-03-01 11:07:45 +0300

    Add Redis Cache placeholder

---
+++ internal/db/infrastructure/cache/redis.go
@@ -0,0 +1,39 @@
+package cache
+
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
+
+type RedisCache struct {
+	client *redis.Client
+}
+
+func NewRedisCache(host, port, password string) (*RedisCache, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     host + ":" + port,
+		Password: password,
+		DB:       0, // Используем базу данных по умолчанию
+	})
+
+	ctx := context.Background()
+	if err := client.Ping(ctx).Err(); err != nil {
+		return nil, err
+	}
+
+	return &RedisCache{client: client}, nil
+}
+
+// Set сохраняет значение в Redis с TTL
+func (r *RedisCache) Set(ctx context.Context, key string, value interface{}, ttl time.Duration) error {
+	return r.client.Set(ctx, key, value, ttl).Err()
+}
+
+func (r *RedisCache) Get(ctx context.Context, key string) (string, error) {
+	return r.client.Get(ctx, key).Result()
+}
+
+func (r *RedisCache) Delete(ctx context.Context, key string) error {
+	return r.client.Del(ctx, key).Err()
+}
